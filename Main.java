23e015b0224be447fbba71eac10bac93d2283427
Max <max@Lyla.(none)>
2013-12-01 14:10:32 -0700

    No longer command line args

--- Main.java
+++ Main.java
@@ -2,11 +2,13 @@
 public class Main {
     
     public static void main(String[] args) {
-        if(args.length < 3) {
-            System.out.println("Usage: SIM M <time (min)> <seed>");
-        } else {
-            new Crosswalk(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-        }
+ //       if(args.length < 3) {
+ //           System.out.println("Usage: SIM M <time (min)> <seed>");
+ //       } else {
+ //           new Crosswalk(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+ //       }
+    	
+    	new Crosswalk(100, 1);
     }
 
 }
